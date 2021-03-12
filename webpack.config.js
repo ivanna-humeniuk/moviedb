23fcbb87b50d2fb7d2f11e95996340299b9339f2
Ivanna Humeniuk <ivannahumeniuk@Ivannas-MacBook-Pro.local>
2021-03-12 15:34:43 +0200

    Created base config for project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            }
+        ]
+    },
+
+    output: {
+        filename: "build.js",
+        path: path.resolve(__dirname, 'build')
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, "build"),
+        openPage: ['./'],
+        hot: true,
+        open: true,
+    }
+}
